d858aa313e0373ff7e9fa3590701282c3b12d5c3
0x41111111 <0x41111111@users.noreply.github.com>
2017-08-17 16:36:35 +1200

    Fix a broken test

--- src/ui/entry.test.js
+++ src/ui/entry.test.js
@@ -4,6 +4,7 @@ import { PasswordEntry } from './entry';
 
 it('should render without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<PasswordEntry login='test' name='test' />, div);
+  ReactDOM.render(<PasswordEntry entry={{ name: "a", login: "b", password: "c" }}
+    dispatchDelete={(id) => console.log("mock delete " + id)} />, div);
 });
 
